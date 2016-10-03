5fb7a38ac7490a4280f11a4b10875c2435f3f913
Danny van der Spuij <d.vanderspuij@coolblue.nl>
2016-10-03 15:36:02 +0200

    YOLO

---
+++ scripts/mysql-data/events.sql
@@ -0,0 +1,31 @@
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 100, now());
+
+
+insert into events (event, score, datetime) VALUES ('buttonPressed', 50, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 50, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 50, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 50, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 50, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 50, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 50, now());
+
+
+insert into events (event, score, datetime) VALUES ('buttonPressed', 25, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 25, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 25, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 25, now());
+insert into events (event, score, datetime) VALUES ('buttonPressed', 25, now());
