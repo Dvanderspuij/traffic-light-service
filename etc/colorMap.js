549f86d1fd61696b8c4f0b4b338856e8f7f5ef98
Danny van der Spuij <d.vanderspuij@coolblue.nl>
2016-10-05 10:19:53 +0200

    GOGOGOGO

---
+++ etc/colorMap.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const map =  {
+  green: {
+    hex: '00B900',
+    rgb: {
+      red:0,
+      green:185,
+      blue:0
+    }
+  },
+  yellow: {
+    hex: 'FF9933',
+    rgb: {
+      red:255,
+      green:153,
+      blue: 51
+    }
+  },
+  red: {
+    hex: 'FF0000',
+    rgb: {
+      red: 255,
+      green: 0,
+      blue: 0
+    }
+  },
+  cborange: {
+    hex: 'FF6600',
+    rgb: {
+      red: 255,
+      green: 105,
+      blue: 0
+      }
+  },
+  cbblue: {
+    hex: '0090E3',
+    rgb: {
+      red: 0,
+      green: 144,
+      blue : 227
+    }
+  }
+};
+
+module.exports = map;
